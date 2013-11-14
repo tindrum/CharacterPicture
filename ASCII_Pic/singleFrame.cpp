a0ce45f211b743056c5974b68f70b318610c650e
Josh Maguregui <joshmaguregui@aol.com>
2013-11-13 16:25:38 -0800

    Working on singleFrame

---
+++ ASCII_Pic/singleFrame.cpp
@@ -0,0 +1,9 @@
+//
+//  singleFrame.cpp
+//  ASCII_Pic
+//
+//  Created by Josh Maguregui on 11/13/13.
+//  Copyright (c) 2013 Daniel Henderson. All rights reserved.
+//
+
+#include "singleFrame.h"
---
+++ ASCII_Pic/singleFrame.h
@@ -0,0 +1,14 @@
+//
+//  singleFrame.h
+//  ASCII_Pic
+//
+//  Created by Josh Maguregui on 11/13/13.
+//  Copyright (c) 2013 Daniel Henderson. All rights reserved.
+//
+
+#ifndef __ASCII_Pic__singleFrame__
+#define __ASCII_Pic__singleFrame__
+
+#include <iostream>
+
+#endif /* defined(__ASCII_Pic__singleFrame__) */
